e5f8855d92edcbcb8456d03c18641d2779900cd3
Yogesh Rathod <48018485+yogeshrathod7@users.noreply.github.com>
2024-08-07 15:12:21 +0530

    Create start_container.sh
    star_container.sh added

---
+++ scripts/start_container.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+# Pull the Docker image from Docker Hub
+docker pull yogeshrathod1137/simple-python-flask-app
+
+# Run the Docker image as a container
+docker run -d -p 5000:5000 yogeshrathod1137/simple-python-flask-app 
